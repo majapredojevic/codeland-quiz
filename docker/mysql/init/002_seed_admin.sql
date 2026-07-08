c7558fa6673628c1562761a0bae4c992dddb7f3d
Maja Predojevic <maja.predojevic95@gmail.com>
2026-07-08 13:04:18 +0200

    Setup development environment and initial database schema

---
+++ docker/mysql/init/002_seed_admin.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password_hash, role, is_active)
+VALUES (
+    'CodeLand Administrator',
+    'admin@codeland.local',
+    '$2y$10$8lGC7h2ZB8Ke8TTXnh5YbuV87nwozRxuRgqZvO8gxBx6qDDvWT5JC',
+    'ADMIN',
+    TRUE
+);
